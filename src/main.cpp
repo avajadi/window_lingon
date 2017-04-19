f699554c90d5e591befd8b987201cab7f11c8fff
Eddie Olsson <eddie.olsson@ri.se>
2017-04-19 11:53:05 +0200

    First files

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * Blink
+ *
+ * Turns on an LED on for one second,
+ * then off for one second, repeatedly.
+ */
+#include "Arduino.h"
+#include "nodemcu.h"
+
+void setup()
+{
+  Serial.begin(115200);
+}
+
+void loop()
+{
+}
---
+++ src/nodemcu.h
@@ -0,0 +1,10 @@
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 2
+#endif
+
+#ifndef LED_ON
+#define LED_ON LOW
+#endif
+#ifndef LED_OFF
+#define LED_OFF HIGH
+#endif
